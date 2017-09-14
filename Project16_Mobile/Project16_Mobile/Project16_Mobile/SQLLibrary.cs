41196d8e73b209cc98da5500018853e5b836392e
Austin Danaj <afdanaj@oakland.edu>
2017-09-13 21:09:59 -0400

    Mobile App and Web API
    Started the projects

---
+++ Project16_Mobile/Project16_Mobile/Project16_Mobile/SQLLibrary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Project16_Mobile
+{
+    class SQLLibrary
+    {
+        private static SQLLibrary instance;
+
+        public static SQLLibrary getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new SQLLibrary();
+            }
+            return instance;
+        }
+        private SQLLibrary()
+        {
+
+        }
+        //POST api/SQL?username={username}&password={password}
+        public async Task<JsonValue> TestConnection()
+        {
+            string url = "http://sqlloginapi.azurewebsites.net/api/SQL?username=" + "test1" + "&password=" + "test1" + "";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "PUT";
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString
+                   ());
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            
+            }
+
+        }
+        /*
+        private void ParseAndDisplay(JsonValue json)
+        {
+            JsonValue login = json["Login"];
+            usern
+            
+        }
+   */
+        
+       
+    }
+}
---
+++ SQLApi/SQLApi/Controllers/SQLController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SQLApi.Models;
+
+namespace SQLApi.Controllers
+{
+    public class SQLController : ApiController
+    {
+
+        LoginEntities csi4999 = new LoginEntities();
+
+
+        [HttpPost]
+        [ActionName("PROJECT_16_REGISTER")]
+        
+        public HttpResponseMessage Project_16_Register(string username, string password)
+        {
+            
+            Login login = new Login();
+            login.Username = username;
+            login.Password = password;
+            csi4999.Logins.Add(login);
+            csi4999.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.Accepted, "Succefully Created");
+            
+        }
+
+        [HttpGet]
+        [ActionName("PROJECT_16_LOGIN")]
+        public HttpResponseMessage Project_16_Login(string username, string password)
+        {
+
+            Login user = csi4999.Logins.Where(x => x.Username == username && x.Password == password).FirstOrDefault();
+            if(user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.Unauthorized, "Please Enter a valid Username and Password");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, user.Username.ToString() + " " +  user.Password.ToString());
+            }
+        }
+
+    
+    }
+}

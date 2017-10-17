ecb229f99b5e4e5c0a16027f1595d5406bf45182
Austin Danaj <austin3soccer@aol.com>
2017-10-17 10:23:13 -0400

    Added to solution

---
+++ Project16_Mobile/Project16_Mobile/Project16_Mobile.Android/UpdateService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Util;
+
+
+namespace Project16_Mobile.Droid
+{
+    [Service]
+    public class UpdateService : Service
+    {
+        public IBinder Binder { get; private set; }
+
+        public override void OnCreate()
+        {
+            // This method is optional to implement
+            base.OnCreate();       
+           
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            // This method must always be implemented
+         
+            return this.Binder;
+        }
+
+        public override bool OnUnbind(Intent intent)
+        {
+            // This method is optional to implement
+       
+            return base.OnUnbind(intent);
+        }
+
+        public override void OnDestroy()
+        {
+            // This method is optional to implement
+           
+            base.OnDestroy();
+        }
+
+        public class LocalBinder : Binder
+        {
+            public LocalBinder(UpdateService service)
+            {
+                this.Service = service;
+            }
+            public UpdateService Service { get; private set; }
+        }
+
+        public void startUpdateTimer()
+        {
+
+        }
+        public void stopUpdateTimer()
+        {
+
+        }
+
+    }
+}

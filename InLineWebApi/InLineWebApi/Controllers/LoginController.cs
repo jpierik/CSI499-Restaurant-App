e96d8163ca53813bfccb3212ac71320ad53678be
Austin Danaj <austin3soccer@aol.com>
2017-10-17 09:31:47 -0400

    Swapped the projects
    Had to add a new project and remove the other due to missing references,
    new project is working on the server

---
+++ InLineWebApi/InLineWebApi/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+
+namespace InLineWebApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public IEnumerable<User> Get()
+        {
+            using (UserModel entities = new UserModel())
+            {
+                return entities.Users.ToList();
+            }
+        }
+        public User Get(int id)
+        {
+            using (UserModel entities = new UserModel())
+            {
+                return entities.Users.FirstOrDefault(e => e.UserId == id);
+            }
+        }
+      
+    }
+}

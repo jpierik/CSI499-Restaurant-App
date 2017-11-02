fcd8a345002424dfb0f149a716e833bcf4dba7e2
David Madurski <djmadurs@oakland.edu>
2017-11-02 10:49:03 -0400

    Adding files to project

---
+++ Project16_Mobile/Project16_Mobile/Project16_Mobile.Android/DealsActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+
+namespace Project16_Mobile.Droid
+{
+    [Activity(Label = "DealsActivity", Theme = "@style/Theme.AppCompat.Light", ParentActivity = typeof(ResturantActivity))]
+    public class DealsActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Deals);
+            // Create your application here
+        }
+    }
+}

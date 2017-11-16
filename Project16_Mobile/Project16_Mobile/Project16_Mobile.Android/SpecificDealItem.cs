0a8eb6689a8327ceadb9708fc634533e1f28d1c0
David Madurski <djmadurs@oakland.edu>
2017-11-16 11:42:57 -0500

    Adding shit I forgot

---
+++ Project16_Mobile/Project16_Mobile/Project16_Mobile.Android/SpecificDealItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Project16_Mobile.Droid
+{
+    public class SpecificDealItem : RelativeLayout
+    {
+        TextView mRestName = null, mDealText = null;
+        RelativeLayout mSelectedDeal = null;
+
+        public string Name { get; set; }
+        public int Index { get; set; }
+
+        public SpecificDealItem(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize(context);
+        }
+        private void Initialize(Context context)
+        {
+
+            LayoutInflater.From(context).Inflate(Resource.Layout.specific_deal_item_layout, this, true);
+            mDealText = (TextView)FindViewById(Resource.Id.dealTextSP);
+            mSelectedDeal = (RelativeLayout)FindViewById(Resource.Id.selectedDealSP);
+            Index = -1;
+            mSelectedDeal.Click += delegate
+            {
+                Intent intent = new Intent(context, typeof(ResturantActivity)/* Insert Reseraunt Activity*/);
+                intent.PutExtra(UpdateService.EXTRA_RNAME, Name);
+                context.StartActivity(intent);
+            };
+        }
+        public void SetName(string name)
+        {
+            mRestName.Text = name;
+        }
+        public void SetText(string text)
+        {
+            mDealText.Text = text;
+        }
+    }
+}
